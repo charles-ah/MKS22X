1bd2ff0beca00e235c672c1414ca5345a7256dff
Charles Zhang <charleszhang2000@gmail.com>
2016-03-08 12:49:23 -0500

    hopefully git is fixed

---
+++ 06Sorts/Driver.java
@@ -0,0 +1,14 @@
+public class Driver
+{
+    public static void main(String[]args)
+    {
+	Sorts test = new Sorts();
+	int[] arr = {3,2,1,9};
+	test.printArray(arr); 
+	test.merge(arr);
+	test.printArray(arr);
+
+    }
+
+    
+}
---
+++ oldMKS22X/02NQueens/QueenBoard.java
@@ -0,0 +1,124 @@
+public class QueenBoard{
+    private int[][]board;
+    
+    public QueenBoard(int size){
+	board = new int[size][size];
+    }
+
+    /**
+     *precondition: board is filled with 0's only.
+     *postcondition: 
+     *-return false, and board is still filled
+     *with 0's for a board that cannot be solved.
+     *-return true, and board is filled with the 
+     *final configuration of the board after adding 
+     *all n queens.
+     */
+    public boolean solve()
+    {
+	return solveH(0);
+	//	return false;
+    }
+
+    /**
+     *Helper method fr solve. 
+     */
+    private boolean solveH(int col){
+	for(int i=0;i<board.length;i++)
+	    {
+		if(board[i][col]==0)
+		    {
+			addQueen(i,col);
+		    
+			if(col==board.length-1)                                                                                                                                                
+			    {
+				return true;                                                                                                                                                          			               }
+			if(solveH(col+1))
+			    {return true;}
+			else{removeQueen(i,col);}
+		    }
+		
+	    }
+
+	return false;
+    }
+
+    public void printSolution(){
+	/**Print the board like toString, except
+	      all negative numbers, and 0's are replaced with '_'
+	         and all 1's are replaced with 'Q'
+	*/
+	System.out.println(toString());
+
+    }
+
+    /********Do Not Edit Below This Line**********************************/
+
+    private boolean addQueen(int row, int col){
+	if(board[row][col] != 0){
+	    return false;
+	}
+	board[row][col] = 1;
+	int offset = 1;
+	while(col+offset < board[row].length){
+	    board[row][col+offset]--;
+	    if(row - offset >= 0){
+		board[row-offset][col+offset]--;
+	    }
+	    if(row + offset < board.length){
+		board[row+offset][col+offset]--;
+	    }
+	    offset++;
+	}
+	return true;
+    }
+
+    private boolean removeQueen(int row, int col){
+	if(board[row][col] != 1){
+	    return false;
+	}
+	board[row][col] = 0;
+	int offset = 1;
+	while(col+offset < board[row].length){
+	    board[row][col+offset]++;
+	    if(row - offset >= 0){
+		board[row-offset][col+offset]++;
+	    }
+	    if(row + offset < board.length){
+		board[row+offset][col+offset]++;
+	    }
+	    offset++;
+	}
+	return true;
+    }
+
+    public String  toString(){
+	String ans = "";
+	for(int r = 0; r < board.length; r++){
+	    for(int c = 0; c < board[0].length; c++){
+		if(board[r][c]==1)
+		    {ans+="Q \t";}
+		else
+		    {
+			ans+= "-\t";
+		    }
+
+	    }
+	    ans+="\n";
+	}
+	return ans;
+    }
+
+    public static void main(String[]args){
+	if(args.length==0)
+	    {
+		throw new IllegalArgumentException("Please specify a size");
+	    }
+	QueenBoard b = new QueenBoard(Integer.parseInt(args[0]));
+
+	System.out.println(b.solve());
+	b.printSolution();
+    }
+
+    
+}
