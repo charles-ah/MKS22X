4893709e88ddbedc90ef73dc27a0413f34da2559
Charles Zhang <charleszhang2000@gmail.com>
2016-03-21 22:56:29 -0400

    optimized linked lists and added generic type

--- 08MyLinkedList/Driver.java
+++ 08MyLinkedList/Driver.java
@@ -3,11 +3,24 @@ public class Driver
 
     public static void main(String[] args)
     {
-	MyLinkedList test = new MyLinkedList();
-	test.add(1);
-	test.add(2);
-	test.add(3);
+	MyLinkedList<String> test = new MyLinkedList<String>();
+
+	test.add("hi");
+	test.add("myself");
+	test.add("charles");
+	for(int i=0;i<test.getSize();i++)
+	    {
+		System.out.println(test.get(i));
+	    }
 	System.out.println(test.toString());
-    }
+	test.add(0,"nope");
+	System.out.println(test);
+	test.add(3,"nope");
+	System.out.println(test);
+	/*
+	test.remove(2);
+	System.out.println(test);
+	*/
+	}
 
 }
--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -1,16 +1,17 @@
-public class MyLinkedList
+public class MyLinkedList<T>
 {
 
     LNode start;
+    LNode end;
     private int size;
 
 
-    public class LNode
+    private class LNode
     {
-	int data;
+	T data;
 	LNode next;
 	
-	public LNode(int val)
+	public LNode(T val)
 	{
 	    //	    System.out.println("creating new node with value"+val);
 	    data=val;
@@ -21,12 +22,12 @@ public class MyLinkedList
 	{
 	    next = node;
 	}
-	public int get()
+	public T get()
 	{
 	    // System.out.println(data);
 	    return data;
 	}
-	public void set(int n)
+	public void set(T n)
 	{
 	    data=n;
 	}
@@ -39,25 +40,112 @@ public class MyLinkedList
     
     public int getSize(){return size;}
     
-    public void add(int newValue)
+    public void add(T newValue)
     {
 	LNode cur=start;
 	//System.out.println("adding"+newValue);
 	if(size==0)
 	    {
 		start = new LNode(newValue);
+		end = start;
 	    }
 	else
 	    {
-		while(cur.next!=null)
-		    {
-			//System.out.println("k");
-			cur = cur.next;
-		    }
-		cur.next=new LNode(newValue);
+		end.next = new LNode(newValue);
+		end = end.next;
 	    }
 	size++;
-	cur = new LNode(newValue);
+	//cur = new LNode(newValue);
+    }
+    
+    public T get(int index)
+    {
+	LNode cur = start;
+	for(int i=0;i<index;i++)
+	    {
+		cur=cur.next;
+	    }
+	return cur.get();
+    }
+
+    public T set(int index,T newValue)
+    {
+	LNode cur =start;
+	for(int i=0;i<index;i++)
+	    {
+		cur=cur.next;
+	    }
+	T old = cur.data;
+	cur.data=newValue;
+	return old;
+    }
+
+    public T remove(int index)
+    {
+	LNode cur=start;
+	
+	if(index==0)
+	    {
+		T old=start.get();
+		start = start.next;
+		size--;
+		return old;
+	    }
+
+	for(int i=0;i<index-1;i++)
+	    {
+		cur=cur.next;
+	    }
+	if(index<size-1)
+	    {
+		LNode befcur = cur;
+		LNode afcur = (cur.next).next;
+		befcur.next = afcur;
+		size--;
+		return cur.get();
+	    }
+	else
+	    {
+		size--;
+		cur.next=null;
+		return cur.get();
+	    }
+	
+	
+    }
+
+    public boolean add(int index,T value)
+    {
+	LNode cur=start;
+	if(index==0)
+	    {
+		LNode newstart = new LNode(value);
+		start=newstart;
+		newstart.next=cur;
+		size++;
+		System.out.println("added to front");
+		return true;
+	    }
+	for(int i=0;i<index-1;i++)
+	    {
+		cur=cur.next;
+	    }
+	if(index<size-1)
+	    {
+		LNode befcur = cur;
+		LNode afcur = cur.next;
+		LNode newcur = new LNode(value);
+		befcur.next=newcur;
+		newcur.next=afcur;
+		size++;
+		return true;
+	    }
+	else
+	    {
+		add(value);
+		return true;
+	    }
+	//	return false;
     }
     
     public String toString()
@@ -66,9 +154,11 @@ public class MyLinkedList
 	LNode cur=start;
 	for(int i=0;i<size;i++)
 	    {
-		//System.out.println("??");
-		//System.out.println(cur.data);
-		ret += cur.get()+",";
+		ret += (cur.get()).toString();
+		if(i<size-1)
+		    {
+			ret+=" ,";
+		    }
 		cur=cur.next;
 	    }
 	
